8be05ec34a61f9f0790eee68bfd20bfc789a84ca
Marina Gruzdeva <marinag1899@mail.ru>
2022-03-29 19:56:31 +0300

    implemented meduza parsing

--- meduza_getter.py
+++ meduza_getter.py
@@ -1,9 +1,8 @@
-import sys
 import meduza
-import dostoevsky
+import re
 
 
-def getMeduzaPubsByKeywords(keywords, maxSize=20):
+def getMeduzaPubsByKeywords(keywords, maxSize=25):
     meduzaUrlPubMap = {}
     for keyword in keywords:
         pubs = meduza.search(keyword)
@@ -14,12 +13,69 @@ def getMeduzaPubsByKeywords(keywords, maxSize=20):
     return list(meduzaUrlPubMap.values())
 
 
-def main() -> int:
-    #keywords = input().split(' ')
-    keywords = ['украина', 'крым', 'война']
-    meduzaPubs = getMeduzaPubsByKeywords(keywords, 5)
-    return 0
+def getLiveContent(pub):
+    content = ""
+    body = pub["content"]
+    if "lead" in body:
+        for block in body["lead"]:
+            if block["type"] == "rich_title":
+                content += block["data"]["first"] + "\n"
+                content += block["data"]["second"] + "\n"
+            if block["type"] == "important_lead":
+                for elem in block["data"]:
+                    if elem["type"] == "ul":
+                        for item in elem["data"]:
+                            content += item + "\n"
+    if ("broadcast" in body) and ("items" in body["broadcast"]):
+        items = body["broadcast"]["items"]
+        for item in list(items.values()):
+            if "blocks" in item:
+                content += parseBlocks(item["blocks"])
+    return content
 
 
-if __name__ == '__main__':
-    sys.exit(main())
+def parseBlocks(blocks):
+    content = ""
+    for block in blocks:
+        if block["type"] == "p" or block["type"] == "h3" or block["type"] == "lead":
+            content += block["data"]
+        if block["type"] == "image":
+            content += block["data"]["caption"] if "caption" in block["data"] else ""
+        if block["type"] == "grouped":
+            content += parseBlocks(block["data"])
+        content += "\n"
+    return content
+
+
+def getPubContent(pub):
+    if pub["layout"] == "live":
+        return getLiveContent(pub)
+    body = pub["content"]
+    content = ""
+    if "head" in body:
+        for item in body["head"]:
+            if item["type"] == "rich_title":
+                content += item["data"]["first"] + "\n"
+                content += item["data"]["second"] + "\n"
+            if item["type"] == "simple_title":
+                content += item["data"]["first"] + "\n"
+    if "blocks" in body:
+        content += parseBlocks(body["blocks"])
+    return content
+
+
+def getMeduzaMentionsByKeywords(keywords):
+    pubs = getMeduzaPubsByKeywords(keywords)
+    mentions = []
+    for pub in pubs:
+        content = getPubContent(pub)
+        content = re.sub("<.*?>", "", content)
+        mentions.append(dict({
+            "url": pub["url"],
+            "title": pub["title"],
+            "timestamp": pub["datetime"],
+            "content": content,
+            "type": "news"
+        }))
+    return mentions
+

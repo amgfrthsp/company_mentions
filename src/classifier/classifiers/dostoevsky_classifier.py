67ab4ffd5da782936781d6079ca4b3c68e091f19
Sofia Semenova-Zvenigorodskaya <sofia.ladypython@gmail.com>
2022-05-19 15:48:50 +0200

    Delete comments

--- src/classifier/classifiers/dostoevsky_classifier.py
+++ src/classifier/classifiers/dostoevsky_classifier.py
@@ -9,22 +9,9 @@ model = FastTextSocialNetworkModel(tokenizer=tokenizer)
 
 def classify(message) -> Verdict:
     verdict = model.predict([message], k=2)[0]
+
     return Verdict(
         positive=verdict.get("positive"),
         neutral=verdict.get("neutral"),
         negative=verdict.get("negative")
     )
-
-# def classify(mentions):
-#     messages = [mention.content for mention in mentions]
-#     verdicts = model.predict(messages, k=2)
-#
-#     classified_mentions = []
-#
-#     for i, verdict in enumerate(verdicts):
-#         classified_mentions.append(ClassifiedMention(
-#             url=mentions[i].url,
-#             positive=verdict.get("positive"),
-#             neutral=verdict.get("neutral"),
-#             negative=verdict.get("negative")))
-#     return classified_mentions
--- src/classifier/classifiers/vader_classifier.py
+++ src/classifier/classifiers/vader_classifier.py
@@ -16,19 +16,3 @@ def classify(message) -> Verdict:
             neutral=verdict.get("neu"),
             negative=verdict.get("neg")
     )
-
-
-# def classify(mentions):
-#     messages = [ts.google(mention.content[:limit_of_length]) for mention in mentions]
-#     verdicts = [analyzer.polarity_scores(message) for message in messages]
-#
-#     classified_mentions = []
-#
-#     for i, verdict in enumerate(verdicts):
-#         classified_mentions.append(ClassifiedMention(
-#             url=mentions[i].url,
-#             positive=verdict.get("positive"),
-#             neutral=verdict.get("neutral"),
-#             negative=verdict.get("negative")))
-#
-#     return classified_mentions
